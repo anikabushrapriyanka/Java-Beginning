d7315689f3735494a89d1aa3387e46e601f881e8
Anika Bushra Priyanka <anikabushrapriyanka@gmail.com>
2025-03-08 12:32:30 +0600

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+ public class HelloWorld{
+	public static void main(String []args){
+	System.out.println("Anika Bushra priyanka")	;
+	int x=123;
+	System.out.println("My num is : " + x);
+	long y= 1234456789023455L;
+	System.out.println("The long num is : "+ y);
+	double z=3.74;
+	System.out.println("My CG is : "+z);
+	float q= 3.1415f;
+	System.out.println("The value of pi is : " + q);
+	boolean a=true;
+	System.out.println("0is greater than -1: "+ a);
+	char letter='P';
+	System.out.println("The first letter of my name is : "+ letter);
+	String name="Orhan";
+	System .out. println("This boys name is : " +name);
+	}
+}
---
+++ MainClass.java
@@ -0,0 +1,8 @@
+public class MainClass {
+	public static void main (String args[]){
+		System.out.println("Hello Everybody");
+		int age=22;
+		System.out.println("My age is : " +(age+age));
+		}
+	}
+	
